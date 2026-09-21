25b8698dd66bd5f915b2ca8f002ff71f15d5d37b
Alex Chen <107821604+xelanehc@users.noreply.github.com>
2026-09-21 12:44:45 -0700

    Add: boats-to-save-people - submission-1

---
+++ Data Structures & Algorithms/boats-to-save-people/submission-1.py
@@ -0,0 +1,27 @@
+class Solution:
+    def numRescueBoats(self, people: List[int], limit: int) -> int:
+        count = [0] * (max(people) + 1)
+
+        for p in people:
+            count[p] += 1
+        
+        p_i, i = 0, 1
+        while p_i < len(people):
+            while count[i] == 0:
+                i += 1
+            people[p_i] = i
+            count[i] -= 1
+            p_i += 1
+        
+        print(people)
+        
+        l, r = 0, len(people) - 1
+        res = 0
+        while l <= r:
+            remaining = limit - people[r]
+            res += 1
+            r -= 1
+            if l <= r and remaining >= people[l]:
+                l += 1
+            
+        return res
